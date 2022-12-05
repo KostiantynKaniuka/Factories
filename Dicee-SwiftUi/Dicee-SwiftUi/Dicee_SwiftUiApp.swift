e3d0f68b93d7c04ec6d9b76dbb5babc2491c23c3
heyhocombo <heyhocombo@gmail.com>
2022-12-05 22:20:20 +0200

    Dicee

---
+++ Dicee-SwiftUi/Dicee-SwiftUi/Dicee_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUiApp.swift
+//  Dicee-SwiftUi
+//
+//  Created by Kostiantyn Kaniuka on 05.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
